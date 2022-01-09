26db97a7a526f3993b965293ae10c066337a289f
Shakthi Visagan <shak360@gmail.com>
2022-01-09 00:23:08 -0800

    more work

--- learning-rust/type-driven-api-design-progress/src/main.rs
+++ learning-rust/type-driven-api-design-progress/src/main.rs
@@ -6,6 +6,8 @@ const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 struct Progress<Iter> {
     iter: Iter,
     i: usize, // usize an unisgned integer where the number of bits depends on the architecture
+    bound: Option<usize>,
+    delims: (char, char),
 }
 
 // the impl block associates a method with a type (the struct)
@@ -13,7 +15,12 @@ struct Progress<Iter> {
 impl<Iter> Progress<Iter> {
     // struct (class?) instantiation method
     pub fn new(iter: Iter) -> Self {
-        Progress { iter, i: 0 }
+        Progress {
+            iter,
+            i: 0,
+            bound: None,
+            delims: ('[', ']'),
+        }
     }
 }
 
@@ -24,22 +31,75 @@ where
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        println!("{}{}", CLEAR, "*".repeat(self.i));
+        print!("{}", CLEAR);
+        match self.bound {
+            Some(bound) => {
+                println!(
+                    "[{}{}{}{}]",
+                    self.delims.0,
+                    "*".repeat(self.i),
+                    " ".repeat(bound - self.i),
+                    self.delims.1
+                );
+            }
+            None => {
+                println!("{}{}{}", self.delims.0, "*".repeat(self.i), self.delims.1);
+            }
+        }
+
         self.i += 1;
         self.iter.next()
     }
 }
 
-fn expensive_calculation(_n: &i32) {
-    sleep(Duration::from_secs(1))
+// add another method when there is a bound
+impl<Iter> Progress<Iter>
+where
+    Iter: ExactSizeIterator,
+{
+    pub fn with_bound(mut self) -> Self {
+        self.bound = Some(self.iter.len());
+        self
+    }
+}
+
+// be able to customize the delimiters
+impl<Iter> Progress<Iter>
+where
+    Iter: ExactSizeIterator,
+{
+    pub fn with_delims(mut self, delims: (char, char)) -> Self {
+        self.delims = delims;
+        self
+    }
+}
+
+trait ProgressIteratorExt: Sized {
+    fn progress(self) -> Progress<Self>;
+}
+
+// for all types Iter, implement the trait ProgressIteratorExt
+// for that quantified type
+// this has to be imported for use in other modules
+impl<Iter> ProgressIteratorExt for Iter
+where
+    Iter: Iterator,
+{
+    fn progress(self) -> Progress<Self> {
+        Progress::new(self)
+    }
 }
 
 fn main() {
+    fn expensive_calculation(_n: &i32) {
+        sleep(Duration::from_secs(1))
+    }
+
     let v: Vec<i32> = vec![1, 2, 3];
     // progress(v.iter(), expensive_calculation);
     // the above is still a little intrusive
 
-    for n in Progress::new(v.iter()) {
+    for n in v.iter().progress().with_bound().with_delims(('<', '>')) {
         expensive_calculation(n);
     }
 
@@ -47,4 +107,14 @@ fn main() {
     h.insert(0);
     // progress(h.iter(), expensive_calculation);
     // the above is still a little intrusive
+
+    for n in h.iter().progress().with_bound() {
+        expensive_calculation(n);
+    }
+
+    // this should error out because with_bound() isn't implemented for iterators without a bound
+    // the trait bound for ExactSizeIterator was not satisfied
+    // for n in (0..).progress().with_bound() {
+    //     expensive_calculation(&n);
+    // }
 }
